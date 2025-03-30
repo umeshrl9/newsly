e69f2fbc3ecadd3ae73fcdd746136f802677319e
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2025-03-30 14:28:23 +0530

    Project complete

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = 3000;
 
@@ -10,6 +12,40 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.render("index.ejs", {year: new Date().getFullYear()});
 });
+
+app.set("view engine", "ejs");
+
+
+const API_KEY = process.env.API_KEY;
+
+app.get("/home", async (req, res) => {
+    const country = req.query.country || "us"; // Default to US
+    const category = req.query.category || ""; // Empty means all categories
+
+
+    try {
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines`, {
+            params: {
+                apiKey: process.env.API_KEY, // Use .env API key
+                country: country,
+                category: category || undefined, // Exclude if empty
+            },
+        });
+
+
+        const topArticles = response.data.articles.filter(
+            (article) => article.urlToImage && article.description
+        );
+
+        console.log(topArticles);
+
+        res.render("home", { articles: topArticles.slice(0,15), year: new Date().getFullYear()});
+    } catch (error) {
+        console.error("Error fetching news:", error.message);
+        res.render("home", { articles: [], year: new Date().getFullYear() });
+    }
+});
+
 
 app.get("/about-us", (req, res) => {
     res.render("about.ejs", {year: new Date().getFullYear()});
